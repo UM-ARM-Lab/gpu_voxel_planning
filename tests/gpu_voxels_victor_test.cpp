31f9b8afe5141ff02fccc7983f51f754cc05ac5d
Brad Saund <brad.saund@gmail.com>
2018-06-08 10:15:45 -0400

    Added tests for adding maps

--- tests/gpu_voxels_victor_test.cpp
+++ tests/gpu_voxels_victor_test.cpp
@@ -37,6 +37,36 @@ TEST(GpuVoxelVictor, collisions)
     // std::cin >> dummy;
 }
 
+TEST(GpuVoxels, addMaps)
+{
+    GpuVoxelsVictor vm;
+    gpu_voxels::GpuVoxelsSharedPtr gvl = vm.gvl;
+    std::string m1 = "TestMap1";
+    std::string m2 = "TestMap2";
+    gvl->addMap(MT_PROBAB_VOXELMAP, m1);
+    gvl->addMap(MT_PROBAB_VOXELMAP, m2);
+
+    EXPECT_EQ(0, vm.countIntersect(m1, m2)) << "two empty maps have an intersection";
+    gvl->insertBoxIntoMap(Vector3f(1.0,0.8,1.0), Vector3f(2.0,1.0,1.2),
+                          m1, PROB_OCCUPIED, 2);
+
+    size_t orig_box_vox = vm.countIntersect(m1, FULL_MAP);
+    EXPECT_TRUE(orig_box_vox > 0) << "Inserted box, but not seeing any voxels";
+
+    vm.getMap(m2)->add(vm.getMap(m1));
+    size_t new_box_vox = vm.countIntersect(m2, FULL_MAP);
+    EXPECT_TRUE(new_box_vox > 0) << "Added map, but not seeing any voxels";
+    EXPECT_EQ(orig_box_vox, new_box_vox) << "Added map, but they have a different number of voxels";
+    EXPECT_EQ(orig_box_vox, vm.countIntersect(m1, m2)) << "added map not identical";
+
+    gvl->clearMap(m2);
+    EXPECT_EQ(0, vm.countIntersect(m2, FULL_MAP)) << "clearning did not clear map 2";
+    vm.getMap(m2)->add(vm.getMap(m1));
+    EXPECT_EQ(orig_box_vox, vm.countIntersect(m2, m1)) << "re-adding map did not work properly";
+    vm.getMap(m2)->add(vm.getMap(m1));
+    EXPECT_EQ(orig_box_vox, vm.countIntersect(m2, m1)) << "re-adding map did not work properly";
+}
+
 
 
 GTEST_API_ int main(int argc, char **argv) {
