5fe2c9937929dd4890774e3cc7a3981a5fcb70d8
Brad Saund <brad.saund@gmail.com>
2018-12-17 11:40:32 -0500

    Added edge weight computation that includes CHS probability of collisions

--- include/gpu_voxel_planning/lazysp_voxels.hpp
+++ include/gpu_voxel_planning/lazysp_voxels.hpp
@@ -6,7 +6,7 @@
 #include "helpers.hpp"
 #include <arc_utilities/timing.hpp>
 #include <graph_planner/dijkstras_addons.hpp>
-
+#include <cmath>
 
 
 bool isValidFromKnownObs(std::vector<double> start, std::vector<double> end,
@@ -18,7 +18,6 @@ bool isValidFromKnownObs(std::vector<double> start, std::vector<double> end,
     {
         victor->addQueryState(victor->toVictorConfig((point.data())));
     }
-
     return victor->countIntersect(VICTOR_QUERY_MAP, KNOWN_OBSTACLES_MAP) == 0;
 }
 
@@ -35,6 +34,36 @@ bool checkEdge(arc_dijkstras::GraphEdge &e, arc_dijkstras::Graph<std::vector<dou
 }
 
 
+double calculateEdgeWeight(arc_dijkstras::Graph<std::vector<double>> &g,
+                           arc_dijkstras::GraphEdge &e,
+                           GpuVoxelsVictor* victor)
+{
+    PROFILE_START("calc_edge_weight");
+    auto start = g.GetNodeMutable(e.GetFromIndex()).GetValueMutable();
+    auto end = g.GetNodeMutable(e.GetToIndex()).GetValueMutable();
+    auto path = interpolatePath(start, end, 0.1);
+    victor->resetQuery();
+    for(auto point: path)
+    {
+        victor->addQueryState(victor->toVictorConfig((point.data())));
+    }
+
+    auto overlaps = victor->countCHSCollisions();
+    auto chs_sizes = victor->chsSizes();
+
+    double p_no_col = 1.0;
+    for(size_t i=0; i<overlaps.size(); i++)
+    {
+        p_no_col *= 1 - ((double)overlaps[i]) / (double)chs_sizes[i];
+    }
+
+    double alpha = 1.0;
+    double chs_cost = -std::log(p_no_col);
+    double l_cost = e.GetWeight();
+    std::cout << "Calculating edge weight took: " << PROFILE_RECORD("calc_edge_weight") << "s\n";
+    return l_cost + alpha * chs_cost;
+}
+
 
 double evaluateEdge(arc_dijkstras::Graph<std::vector<double>> &g,
                     arc_dijkstras::GraphEdge &e,
@@ -44,7 +73,7 @@ double evaluateEdge(arc_dijkstras::Graph<std::vector<double>> &g,
     {
         checkEdge(e, g, victor);
     }
-    return e.GetWeight();
+    return calculateEdgeWeight(g, e, victor);
 }
 
 
