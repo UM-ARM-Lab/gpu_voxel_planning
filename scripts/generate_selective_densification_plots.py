d25968c5ffaaf55720d374ec94420317ada9e1a7
Brad Saund <brad.saund@gmail.com>
2019-03-07 16:30:26 -0500

    Added plots to plotting script

--- scripts/generate_selective_densification_plots.py
+++ scripts/generate_selective_densification_plots.py
@@ -8,15 +8,22 @@ logged_filepath = "/home/bradsaund/catkin_ws/src/gpu_voxel_planning/selective_de
 save_path = logged_filepath
 
 
+color_cycle = ['#332288', '#88CCEE', '#44AA99', '#117733', '#999933', '#DDCC77', '#CC6677', '#882255', '#AA4499']
+color_cycle = ['#e41a1c',
+               '#377eb8',
+               '#4daf4a',
+               '#984ea3',
+               '#ff7f00',
+               '#ffff33',
+               '#a65628',
+               '#f781bf']
 
 strategy_mappings = {"RRT_Strategy":"RRT",
-                     "Omniscient_SD_Graph_Search": "Selective Densification",
-                     "Omniscient_SD_Graph_Search_": "Selective Densification",
-                     "Omniscient_SD_Graph_Search_precomputed": "SD with precomputed edges",
-                     "Omniscient_SD_Graph_Search_precomputed_": "SD with precomputed edges",
+                     "Omniscient_SD_Graph_Search": "SD",
+                     "Omniscient_SD_Graph_Search_precomputed": "SD-pre",
                      "BIT_Strategy":"BIT*",
-                     "Dense_Graph_Search":"Dense Graph",
-                     "Dense_Graph_Search_precomputed":"DG with precomputed edges"
+                     "Dense_Graph_Search":"DG",
+                     "Dense_Graph_Search_precomputed":"DG-pre"
                      }
 
 scenario_mappings = {"Bookshelf":"Bookshelf",
@@ -25,19 +32,19 @@ scenario_mappings = {"Bookshelf":"Bookshelf",
 
 ordering = {"RRT": 1,
             "BIT*":1.1,
-            "Dense Graph":1.8,
-            "DG with precomputed edges":1.9,
-            "Selective Densification":2,
-            "SD with precomputed edges":3
+            "DG":1.8,
+            "DG-pre":1.9,
+            "SD":2,
+            "SD-pre":3
             }
 
 
-linestyles = {"RRT": {"color": "grey", "linewidth":2},
-              "BIT*": {"color" : [1, 0, 0], "linewidth":2},
-              "Dense Graph":{"color" : [.3, .7, .1], "linewidth":2},
-              "DG with precomputed edges":{"color" : [.3, .7, .6], "linewidth":2},
-              "Selective Densification": {"color": "blue", "linewidth":4},
-              "SD with precomputed edges": {"color": "green", "linewidth":4}}
+linestyles = {"RRT":    {"color": "grey",         "linewidth":2},
+              "BIT*":   {"color": color_cycle[3], "linewidth":2},
+              "DG":     {"color": "yellow",       "linewidth":2},
+              "DG-pre": {"color": "orange",       "linewidth":2},
+              "SD":     {"color": "blue",         "linewidth":5},
+              "SD-pre": {"color": "green",        "linewidth":5}}
 
 
 
@@ -52,14 +59,21 @@ def extract_timing_data(dirname, filename):
     filepath = dirname + filename
     t = []
     l = []
+    u = []
+    c_p = -1
     eps = 0.0001
     with open(filepath, "r") as f:
         # line = f.readline()
         lines = f.readlines()
         for line in lines:
+            data = line.split()
+            if line.startswith("c_p") and len(data) < 5:
+                c_p = float(data[2])
+                continue
+            
             if not line.startswith("PathLength"):
                 continue
-            data = line.split()
+
             if len(data) > 3:
                 continue
 
@@ -76,7 +90,12 @@ def extract_timing_data(dirname, filename):
 
             
             # print(data)
-    return [t, l]
+    u = [time + length*4 for (time, length) in zip(t, l)]
+            
+    return {"time": t,
+            "length": l,
+            "utility": u,
+            "c_p": c_p}
 
 def load_experiment_files():
 
@@ -89,7 +108,12 @@ def load_experiment_files():
     return exps
 
 
-def plot_group(exps):
+
+def plot_all_strategies(exps, variant):
+    """plots strategies on the same plot
+    exps: list of experiement data
+    variant: string, either "length" or "utility"
+    """
     scenario = exps[0]["scenario"];
     print "Scenario:", scenario
 
@@ -101,17 +125,34 @@ def plot_group(exps):
     
     for exp in exps:
         data = exp["data"]
-        # ax.plot(data[0], data[1], label=exp["strategy"])
-        ax.plot(data[0], data[1], label=exp["strategy"], **linestyles[exp["strategy"]])
-    plt.title(scenario)
 
-    ax.set_xlabel("Planning Time (s)")
-    ax.set_ylabel("Path Length (rad)")
+        # if(exp["data"]["c_p"] != -1 and
+        #    exp["data"]["c_p"] != 1):
+        #     continue
+            
+
+        if(variant == "length"):
+            y = data["length"]
+            y_axis_label = "Path Length (rad)"
+        if(variant == "utility"):
+            y = data["utility"]
+            y_axis_label = "Planning + Execution (s)"
+            # ax.set_ylim([0, 50])
+            ax.set_xlim([0, 50])
+
+        if(len(data["time"]) > 0 and data["time"][0] > 100):
+            continue
+            
+        ax.plot(data["time"], y, label=exp["strategy"], **linestyles[exp["strategy"]])
+    # plt.title(scenario)
+
+    ax.set_xlabel("Planning Time (s)", fontsize=22)
+    ax.set_ylabel(y_axis_label, fontsize=22)
+    ax.xaxis.set_tick_params(labelsize=20)
+    ax.yaxis.set_tick_params(labelsize=20)
 
     box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.5, box.height])
-
-    
+    ax.set_position([box.x0, box.y0 + 0.05, box.width, box.height])
 
     handles, labels = ax.get_legend_handles_labels()
     handle_list, label_list = [], []
@@ -119,17 +160,88 @@ def plot_group(exps):
         if label not in label_list:
             handle_list.append(handle)
             label_list.append(label)
-    ax.legend(handle_list, label_list, loc='center left', bbox_to_anchor=(1, 0.5))
-
-
+    # ax.legend(handle_list, label_list, loc='center left', bbox_to_anchor=(1.1, 0.5))
     
     # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    
-    plt.savefig(save_path + scenario)
+    plt.savefig(save_path + scenario + "_" + variant)
     plt.show()
+
+
+    fig_legend = plt.figure()
+    fig_legend.legend(handle_list, label_list)
+    fig_legend.savefig(save_path + scenario + "_legend", bbox_inches = 'tight')
+    plt.show()
+    
     # print exps
     # print len(exps)
     # print ""
+
+    
+
+def plot_without_smoothing(exps):
+    exps.sort(key=lambda exp: ordering[exp["strategy"]])
+    scenario = exps[0]["scenario"];
+
+    fig = plt.figure()
+    ax = plt.subplot(211)
+    ax2 = plt.subplot(212)
+
+    for exp in exps:
+        # if(exp["strategy"] != "Selective Densification" and
+        #    exp["strategy"] != "SD with precomputed edges"):
+        if(exp["strategy"] != "SD-pre"):
+            continue
+        data = exp["data"]
+        # x = data["time"][0]
+        x = data["c_p"]
+        y = data["length"][0]
+        ax.scatter(x, y, label=exp["strategy"], **linestyles[exp["strategy"]])
+        ax2.scatter(x, data["time"][0], label=exp["strategy"], **linestyles[exp["strategy"]])
+        # ax.annotate(exp["data"]["c_p"], (x, y))
+
+
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width, box.height])
+
+    box = ax2.get_position()
+    ax2.set_position([box.x0, box.y0 + .03, box.width, box.height])
+
+
+    
+    ax.set_xscale("symlog", linthreshx=0.00001)
+    ax.set_xlim([-0.00001, 1000])
+    # ax.set_xlabel("Planning Time (s)")
+    ax.set_ylabel("Path Length (rad)", fontsize=22)
+    ax.yaxis.set_label_coords(-0.1, 0.6)
+    # ax.title(scenario)
+
+    ax2.set_xscale("symlog", linthreshx=0.00001)
+    # ax2.set_yscale("log")
+    ax2.set_xlim([-0.00001, 1000])
+    ax2.set_ylabel("Planning time (s)", fontsize=22)
+    ax2.set_xlabel("c_p", fontsize = 22)
+
+    
+    ax2.set_xticks([0, 0.0001, 0.01, 1, 100])
+    ax2.xaxis.set_tick_params(labelsize=20)
+    ax2.yaxis.set_tick_params(labelsize=20)
+    ax2.set_yticks(ax2.get_yticks()[::2])
+    ax2.yaxis.set_label_coords(-0.1, 0.4)
+    
+    ax.set_xticklabels([])
+    ax.set_yticks(ax.get_yticks()[::2])
+    ax.yaxis.set_tick_params(labelsize=20)
+    
+    # plt.title(scenario)
+    plt.savefig(save_path + scenario + "_cp_sweep")
+    plt.show()
+        
+
+def plot_group(exps):
+    plot_all_strategies(exps, "length")
+    plot_all_strategies(exps, "utility")
+    plot_without_smoothing(exps)
+    
 
 
 def plot_all(exps):
