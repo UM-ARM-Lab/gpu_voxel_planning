f2f45a806b977394a597bf289799924bc697e781
Brad Saund <brad.saund@gmail.com>
2019-03-13 12:33:39 -0400

    Add table output to generate_plots script

--- scripts/generate_selective_densification_plots.py
+++ scripts/generate_selective_densification_plots.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from matplotlib import pyplot as plt
 import os
+import numpy as np
 import IPython
 
 
 logged_filepath = "/home/bradsaund/catkin_ws/src/gpu_voxel_planning/selective_densification_timings/"
 save_path = logged_filepath
+
+plotted_cp = 1.0
 
 
 color_cycle = ['#332288', '#88CCEE', '#44AA99', '#117733', '#999933', '#DDCC77', '#CC6677', '#882255', '#AA4499']
@@ -103,6 +106,8 @@ def load_experiment_files():
     for name in os.listdir(logged_filepath):
         if name.endswith(".png"):
             continue
+        if name.endswith(".txt"):
+            continue
         # extract_timing_data(logged_filepath, name)
         exps.append(extract_experiment_info(logged_filepath, name))
     return exps
@@ -126,9 +131,9 @@ def plot_all_strategies(exps, variant):
     for exp in exps:
         data = exp["data"]
 
-        # if(exp["data"]["c_p"] != -1 and
-        #    exp["data"]["c_p"] != 1):
-        #     continue
+        if(exp["data"]["c_p"] != -1 and
+           exp["data"]["c_p"] != plotted_cp):
+            continue
             
 
         if(variant == "length"):
@@ -253,13 +258,93 @@ def plot_all(exps):
                 continue
             scen_exps.append(exp)
         plot_group(scen_exps)
+
+def get_min_times(exps):
+    min_times = []
+    min_lengths = []
+    min_utils = []
+    for exp in exps:
+        min_time = float("inf")
+        min_length = float("inf")
+        min_util = float("inf")
+        if len(exp["data"]["time"]) > 0:
+            min_time = min(exp["data"]["time"])
+        if len(exp["data"]["length"]) > 0:
+            min_length = min(exp["data"]["length"])
+        if len(exp["data"]["utility"]) > 0:
+            min_util = min(exp["data"]["utility"])
+        min_times.append(min_time)
+        min_lengths.append(min_length)
+        min_utils.append(min_util)
+    return {"min_times" : min_times,
+            "min_lengths" : min_lengths,
+            "min_utils" : min_utils}
+    
+
+def print_best_scene_strat_stats(exps, f):
+    """
+    Takes in experiments all from the same scene/strat and a file f
+    Prints the min to line
+    """
+    r = get_min_times(exps)
+    
+    f.write("    & " + str(min(r["min_times"]))
+            + " & "  + str(min(r["min_lengths"]))
+            + " & "  + str(min(r["min_utils"])))
+    f.write("\n")
+
+def print_avg_scene_strat_stats(exps, f):
+    """
+    Takes in experiments all from the same scene/strat and a file f
+    Prints the min to line
+    """
+    r = get_min_times(exps)
+    
+    f.write("    & " + str(mean(r["min_times"]))
+            + " & "  + str(mean(r["min_lengths"]))
+            + " & "  + str(mean(r["min_utils"])))
+    f.write("\n")
+
+
+
+
+    
+def print_stats(exps):
+    # scenarios = {x["scenario"] for x in exps}
+    strategies = {x["strategy"] for x in exps}
+    filepath = logged_filepath + "stats.txt"
+
+    ordered_scenarios = ["Table", "Bookshelf", "Wall"]
+
+    def is_valid(exp, scenario, strat):
+        if not exp["scenario"] == scenario:
+            return False
+        if not exp["strategy"] == strat:
+            return False
+        if(exp["data"]["c_p"] != -1 and
+           exp["data"]["c_p"] != plotted_cp):
+            return False
+        return True
+        
+    
+    
+    with open(filepath, "w") as f:
+        for strat in strategies:
+            f.write(strat + " \n")
+            for scenario in ordered_scenarios:
+                matching_exps = [exp for exp in exps if is_valid(exp, scenario, strat)]
+                f.write("  % " + scenario  +"\n")
+                print_best_scene_strat_stats(matching_exps, f)
+            f.write("  \\\\ \n")
+
     
 
 if __name__ == "__main__":
     
     print("hi")
     exps = load_experiment_files()
-    plot_all(exps)
+    # plot_all(exps)
+    print_stats(exps)
     
 
 
