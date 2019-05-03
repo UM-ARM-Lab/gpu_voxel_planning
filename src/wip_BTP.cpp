005cb5eba4c087719319a6c0a8142f24b4f0f463
Brad Saund <brad.saund@gmail.com>
2019-05-02 21:52:22 -0400

    Save SV to file after trials

--- src/wip_BTP.cpp
+++ src/wip_BTP.cpp
@@ -92,6 +92,8 @@ int main(int argc, char* argv[])
     tester.attemptStrategy(strat);
     // strat.saveToFile();
 
+    strat.saveToFile("/home/bradsaund/catkin_ws/src/gpu_voxel_planning/graphs/swept_volumes_100k.map");
+
     // viz.vizEEGraph(strat.graph);
     // viz.vizEESDGraph(strat.sd_graph);
     
