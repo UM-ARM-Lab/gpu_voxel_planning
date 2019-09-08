ef7ad3e988d1afd0fce8179fa11848350b031356
Brad Saund <brad.saund@gmail.com>
2019-09-08 13:29:41 -0400

    Generalized roadmap for later reuse

--- src/strategies/victor_halton_roadmap.cpp
+++ src/strategies/victor_halton_roadmap.cpp
@@ -3,6 +3,29 @@
 #include <arc_utilities/timing.hpp>
 
 
+static double getConnectionRadius(int num_nodes, int dim, double num_neighbors_desired)
+{
+    if(dim != 7)
+    {
+        throw std::invalid_argument("radiusAtDepth only writted to accomodate dimension 7");
+    }
+    
+    double num_points = num_nodes;
+    double space_volume = 1.0;
+
+    for(int i=0; i<right_joint_lower_deg.size(); i++)
+    {
+        space_volume *= (right_joint_upper_deg[i] - right_joint_lower_deg[i])*torad;
+    }
+
+    double connection_ball_volume = (double)num_neighbors_desired * space_volume / num_points;
+    double connection_radius = 0.8 * std::pow(connection_ball_volume, 1.0/dim);
+    // https://en.wikipedia.org/wiki/Volume_of_an_n-ball
+
+    return connection_radius;
+}
+
+
 
 
 Roadmap::Roadmap():
@@ -39,6 +62,28 @@ Roadmap::Roadmap():
     std::cout << "Made graph with " << nodes_.size() << " vertices and " << countEdges() << " edges\n";
 }
 
+Roadmap::Roadmap(int num_nodes, double approx_edges_per_node):
+    HaltonGraph(0,0,0)
+{
+    r_disc = getConnectionRadius(num_nodes, 7, approx_edges_per_node);
+
+    auto configs = scaleToVictorDims(halton::haltonPoints(num_nodes, 7));
+
+    PROFILE_START("node_creation");
+    int i=0;
+    for(auto q:configs)
+    {
+        addVertexAndEdges(q);
+        i++;
+        if(i%10000 == 0)
+        {
+            std::cout << i << "/" << num_nodes << " nodes in " <<
+                PROFILE_RECORD("node_creation") << "s\n";
+        }
+    }
+    std::cout << "Made graph with " << nodes_.size() << " vertices and " << countEdges() << " edges\n";
+
+}
 
 Roadmap::Roadmap(std::string filename):
     HaltonGraph(0,0,0)
