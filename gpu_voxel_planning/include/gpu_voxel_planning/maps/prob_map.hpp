7b5202a10d2ad1bc0b38fe12d03ebe3fcea319f9
Brad Saund <brad.saund@gmail.com>
2020-12-11 14:25:00 -0500

    Clean up probmap by using base class methods directly

--- gpu_voxel_planning/include/gpu_voxel_planning/maps/prob_map.hpp
+++ gpu_voxel_planning/include/gpu_voxel_planning/maps/prob_map.hpp
@@ -9,98 +9,57 @@
 #define GRID_Z_DIM 256
 #define VOXEL_SIDE_LENGTH 0.02
 
-
-
 class DenseGrid;
-
 
 /***************************
  *   Sparse Grid
  ****************************/
-class SparseGrid: private gpu_voxels::voxellist::ProbVoxelList
-{
-public:
-    SparseGrid();
+class SparseGrid : private gpu_voxels::voxellist::ProbVoxelList {
+ public:
+  SparseGrid();
+  explicit SparseGrid(const DenseGrid& other);
+  const gpu_voxels::voxellist::ProbVoxelList* getProbVoxelList() const;
+  SparseGrid& operator=(const DenseGrid& other);
+  void merge(const DenseGrid* other);
 
-    SparseGrid(const DenseGrid &other);
-
-    const gpu_voxels::voxellist::ProbVoxelList* getProbVoxelList() const;
-
-    SparseGrid& operator=(const DenseGrid &other);
-
-    void merge(const DenseGrid* other);
-
-    float getVoxelSideLength() const override;
-
-    uint64_t serializeSelf(std::vector<uint8_t>& buffer) const;
-    
-    bool deserializeSelf(std::vector<uint8_t>& buffer, uint64_t &buffer_index);
-
-    bool writeToDisk(const std::string& path);
-
-    bool readFromDisk(const std::string& path);
-
+  using gpu_voxels::voxellist::ProbVoxelList::getVoxelSideLength;
+  using gpu_voxels::voxellist::ProbVoxelList::serializeSelf;
+  using gpu_voxels::voxellist::ProbVoxelList::deserializeSelf;
+  using gpu_voxels::voxellist::ProbVoxelList::writeToDisk;
+  using gpu_voxels::voxellist::ProbVoxelList::readFromDisk;
 };
-
-
-
 
 /***************************
  *   Dense Grid
  ****************************/
 
-class DenseGrid : private gpu_voxels::voxelmap::ProbVoxelMap
-{
-public:
-    DenseGrid();
-    
-    DenseGrid(const DenseGrid &other);
+class DenseGrid : private gpu_voxels::voxelmap::ProbVoxelMap {
+ public:
+  DenseGrid();
+  DenseGrid(const DenseGrid& other);
+  explicit DenseGrid(const SparseGrid& other);
+  const gpu_voxels::voxelmap::ProbVoxelMap* getProbVoxelMap() const;
+  DenseGrid& operator=(const DenseGrid& other);
+  void insertBox(const Vector3f& corner_min, const Vector3f& corner_max);
+  void copyRandomOccupiedElement(DenseGrid& to) const;
+  bool overlapsWith(const DenseGrid* other, float coll_threshold = 1.0) const;
+  size_t collideWith(const DenseGrid* map);
+  void subtract(const DenseGrid* other);
+  void add(const DenseGrid* other);
+  void copy(const DenseGrid* other);
+  void merge(const SparseGrid* other);
+  void copyIthOccupied(const DenseGrid* other, unsigned long copy_index) const;
 
-    explicit DenseGrid(const SparseGrid &other);
-
-    const gpu_voxels::voxelmap::ProbVoxelMap* getProbVoxelMap() const;
-
-    DenseGrid& operator=(const DenseGrid &other);
-
-    void insertBox(const Vector3f &corner_min, const Vector3f &corner_max);
-
-    void copyRandomOccupiedElement(DenseGrid& to) const;
-
-    bool overlapsWith(const DenseGrid* other, float coll_threshold = 1.0) const;
-
-    size_t collideWith(const DenseGrid* map);
-
-    void subtract(const DenseGrid *other);
-
-    void add(const DenseGrid *other);
-
-    void copy(const DenseGrid *other);
-
-    void merge(const SparseGrid *other);
-
-    size_t countOccupied() const;
-    
-    void copyIthOccupied(const DenseGrid* other, unsigned long copy_index) const;
-
-    void insertMetaPointCloud(const MetaPointCloud &meta_point_cloud, BitVoxelMeaning voxel_meaning) override;
-
-    void clearMap() override;
-
-    void insertPointCloud(const std::vector<Vector3f> &point_cloud, const BitVoxelMeaning voxel_meaning) override;
-
-    void insertPointCloud(const PointCloud &pointcloud, const BitVoxelMeaning voxel_meaning) override;
-
-    std::vector<Vector3f> getOccupiedCenters() const;
-    std::vector<Vector3ui> getOccupiedCoords() const;
-
-    float getVoxelSideLength() const override;
-
-    Vector3ui getDimensions() const override;
-
-    bool writeToDisk(const std::string& path);
-
-    bool readFromDisk(const std::string& path);
+  using gpu_voxels::voxelmap::ProbVoxelMap::getVoxelSideLength;
+  using gpu_voxels::voxelmap::ProbVoxelMap::getOccupiedCenters;
+  using gpu_voxels::voxelmap::ProbVoxelMap::getOccupiedCoords;
+  using gpu_voxels::voxelmap::ProbVoxelMap::getDimensions;
+  using gpu_voxels::voxelmap::ProbVoxelMap::clearMap;
+  using gpu_voxels::voxelmap::ProbVoxelMap::countOccupied;
+  using gpu_voxels::voxelmap::ProbVoxelMap::insertPointCloud;
+  using gpu_voxels::voxelmap::ProbVoxelMap::insertMetaPointCloud;
+  using gpu_voxels::voxelmap::ProbVoxelMap::writeToDisk;
+  using gpu_voxels::voxelmap::ProbVoxelMap::readFromDisk;
 };
-
 
 #endif
--- gpu_voxel_planning/src/maps/prob_map.cpp
+++ gpu_voxel_planning/src/maps/prob_map.cpp
@@ -1,206 +1,83 @@
 #include "gpu_voxel_planning/maps/prob_map.hpp"
-#include <random>
 
+#include <random>
 
 /********************
  ** Sparse Grid
  *******************/
-SparseGrid::SparseGrid() : gpu_voxels::voxellist::ProbVoxelList(
-    Vector3ui(GRID_X_DIM, GRID_Y_DIM, GRID_Z_DIM),
-    VOXEL_SIDE_LENGTH, MT_PROBAB_VOXELLIST)
-{}
+SparseGrid::SparseGrid()
+    : gpu_voxels::voxellist::ProbVoxelList(Vector3ui(GRID_X_DIM, GRID_Y_DIM, GRID_Z_DIM), VOXEL_SIDE_LENGTH,
+                                           MT_PROBAB_VOXELLIST) {}
 
-SparseGrid::SparseGrid(const DenseGrid &other) :
-    gpu_voxels::voxellist::ProbVoxelList(other.getDimensions(),
-                                         other.getVoxelSideLength(), MT_PROBAB_VOXELLIST)
-{
-    merge(&other);
+SparseGrid::SparseGrid(const DenseGrid& other)
+    : gpu_voxels::voxellist::ProbVoxelList(other.getDimensions(), other.getVoxelSideLength(), MT_PROBAB_VOXELLIST) {
+  merge(&other);
 }
 
-const gpu_voxels::voxellist::ProbVoxelList* SparseGrid::getProbVoxelList() const
-{
-    return this;
+const gpu_voxels::voxellist::ProbVoxelList* SparseGrid::getProbVoxelList() const { return this; }
+
+SparseGrid& SparseGrid::operator=(const DenseGrid& other) {
+  merge(&other);
+  return *this;
 }
 
-
-SparseGrid& SparseGrid::operator=(const DenseGrid &other)
-{
-    merge(&other);
-    return *this;
+void SparseGrid::merge(const DenseGrid* other) {
+  gpu_voxels::voxellist::ProbVoxelList::merge(other->getProbVoxelMap());
 }
-
-void SparseGrid::merge(const DenseGrid* other)
-{
-    gpu_voxels::voxellist::ProbVoxelList::merge(other->getProbVoxelMap());
-}
-
-float SparseGrid::getVoxelSideLength() const
-{
-    return gpu_voxels::voxellist::ProbVoxelList::getVoxelSideLength();
-}
-
-uint64_t SparseGrid::serializeSelf(std::vector<uint8_t>& buffer) const
-{
-    return gpu_voxels::voxellist::ProbVoxelList::serializeSelf(buffer);
-}
-    
-bool SparseGrid::deserializeSelf(std::vector<uint8_t>& buffer, uint64_t &buffer_index)
-{
-    return gpu_voxels::voxellist::ProbVoxelList::deserializeSelf(buffer, buffer_index);
-}
-
-bool SparseGrid::writeToDisk(const std::string& path)
-{
-    return voxellist::ProbVoxelList::writeToDisk(path);
-}
-
-bool SparseGrid::readFromDisk(const std::string& path)
-{
-    return voxellist::ProbVoxelList::readFromDisk(path);
-}
-
-
-
-
 
 /*******************
  ** Dense Grid
  ******************/
-DenseGrid::DenseGrid() : gpu_voxels::voxelmap::ProbVoxelMap(Vector3ui(GRID_X_DIM, GRID_Y_DIM, GRID_Z_DIM),
-                                                            VOXEL_SIDE_LENGTH, MT_PROBAB_VOXELMAP)
-{
+DenseGrid::DenseGrid()
+    : gpu_voxels::voxelmap::ProbVoxelMap(Vector3ui(GRID_X_DIM, GRID_Y_DIM, GRID_Z_DIM), VOXEL_SIDE_LENGTH,
+                                         MT_PROBAB_VOXELMAP) {}
+
+DenseGrid::DenseGrid(const DenseGrid& other)
+    : gpu_voxels::voxelmap::ProbVoxelMap(other.getDimensions(), other.getVoxelSideLength(), MT_PROBAB_VOXELMAP) {
+  copy(&other);
 }
 
-DenseGrid::DenseGrid(const DenseGrid &other) :
-    gpu_voxels::voxelmap::ProbVoxelMap(other.getDimensions(),
-                                       other.getVoxelSideLength(), MT_PROBAB_VOXELMAP)
-{
-    copy(&other);
+DenseGrid::DenseGrid(const SparseGrid& other)
+    : gpu_voxels::voxelmap::ProbVoxelMap(Vector3ui(GRID_X_DIM, GRID_Y_DIM, GRID_Z_DIM), other.getVoxelSideLength(),
+                                         MT_PROBAB_VOXELMAP) {
+  merge(&other);
 }
 
-DenseGrid::DenseGrid(const SparseGrid &other) :
-    gpu_voxels::voxelmap::ProbVoxelMap(Vector3ui(GRID_X_DIM, GRID_Y_DIM, GRID_Z_DIM),
-                                       other.getVoxelSideLength(), MT_PROBAB_VOXELMAP)
-{
-    merge(&other);
+DenseGrid& DenseGrid::operator=(const DenseGrid& other) {
+  copy(&other);
+  return *this;
 }
 
-DenseGrid& DenseGrid::operator=(const DenseGrid &other)
-{
-    copy(&other);
-    return *this;
+const gpu_voxels::voxelmap::ProbVoxelMap* DenseGrid::getProbVoxelMap() const { return this; }
+
+void DenseGrid::insertBox(const Vector3f& corner_min, const Vector3f& corner_max) {
+  float delta = VOXEL_SIDE_LENGTH / 2.0;
+  insertPointCloud(geometry_generation::createBoxOfPoints(corner_min, corner_max, delta), eBVM_OCCUPIED);
 }
 
-const gpu_voxels::voxelmap::ProbVoxelMap* DenseGrid::getProbVoxelMap() const
-{
-    return this;
+void DenseGrid::copyRandomOccupiedElement(DenseGrid& to) const {
+  size_t num_occupied = countOccupied();
+  std::mt19937 generator;
+  generator.seed(std::random_device()());
+  std::uniform_int_distribution<unsigned long> dist(0, num_occupied);
+  unsigned long rand_index = dist(generator);
+  copyIthOccupied(&to, rand_index);
 }
 
-void DenseGrid::insertBox(const Vector3f &corner_min, const Vector3f &corner_max)
-{
-    float delta = VOXEL_SIDE_LENGTH / 2.0;
-    insertPointCloud(geometry_generation::createBoxOfPoints(corner_min, corner_max, delta),
-                     eBVM_OCCUPIED);
+bool DenseGrid::overlapsWith(const DenseGrid* other, float coll_threshold) const {
+  return voxelmap::ProbVoxelMap::overlapsWith(other, coll_threshold);
 }
 
-void DenseGrid::copyRandomOccupiedElement(DenseGrid& to) const
-{
-    size_t num_occupied = countOccupied();
-    std::mt19937 generator;
-    generator.seed(std::random_device()());
-    std::uniform_int_distribution<unsigned long> dist(0, num_occupied);
-    unsigned long rand_index = dist(generator);
-    copyIthOccupied(&to, rand_index);
-}
+size_t DenseGrid::collideWith(const DenseGrid* map) { return voxelmap::ProbVoxelMap::collideWith(map); }
 
+void DenseGrid::subtract(const DenseGrid* other) { voxelmap::ProbVoxelMap::subtract(other); }
 
-bool DenseGrid::overlapsWith(const DenseGrid* other, float coll_threshold) const
-{
-    return voxelmap::ProbVoxelMap::overlapsWith(other, coll_threshold);
-}
+void DenseGrid::add(const DenseGrid* other) { voxelmap::ProbVoxelMap::add(other); }
 
-size_t DenseGrid::collideWith(const DenseGrid* map)
-{
-    return voxelmap::ProbVoxelMap::collideWith(map);
-}
+void DenseGrid::copy(const DenseGrid* other) { voxelmap::ProbVoxelMap::copy(other); }
 
-void DenseGrid::subtract(const DenseGrid *other)
-{
-    voxelmap::ProbVoxelMap::subtract(other);
-}
+void DenseGrid::merge(const SparseGrid* other) { voxelmap::ProbVoxelMap::merge(other->getProbVoxelList()); }
 
-void DenseGrid::add(const DenseGrid *other)
-{
-    voxelmap::ProbVoxelMap::add(other);
-}
-
-void DenseGrid::copy(const DenseGrid *other)
-{
-    voxelmap::ProbVoxelMap::copy(other);
-}
-
-void DenseGrid::merge(const SparseGrid *other)
-{
-    voxelmap::ProbVoxelMap::merge(other->getProbVoxelList());
-}
-
-size_t DenseGrid::countOccupied() const
-{
-    return voxelmap::ProbVoxelMap::countOccupied();
-}
-    
-void DenseGrid::copyIthOccupied(const DenseGrid* other, unsigned long copy_index) const
-{
-    voxelmap::ProbVoxelMap::copyIthOccupied(other, copy_index);
-}
-
-void DenseGrid::insertMetaPointCloud(const MetaPointCloud &meta_point_cloud, BitVoxelMeaning voxel_meaning)
-{
-    voxelmap::ProbVoxelMap::insertMetaPointCloud(meta_point_cloud, voxel_meaning);
-}
-
-void DenseGrid::clearMap()
-{
-    voxelmap::ProbVoxelMap::clearMap();
-}
-
-void DenseGrid::insertPointCloud(const std::vector<Vector3f> &point_cloud, const BitVoxelMeaning voxel_meaning)
-{
-    voxelmap::ProbVoxelMap::insertPointCloud(point_cloud, voxel_meaning);
-}
-
-void DenseGrid::insertPointCloud(const PointCloud &point_cloud, const BitVoxelMeaning voxel_meaning)
-{
-    voxelmap::ProbVoxelMap::insertPointCloud(point_cloud, voxel_meaning);
-}
-
-std::vector<Vector3f> DenseGrid::getOccupiedCenters() const
-{
-    return voxelmap::ProbVoxelMap::getOccupiedCenters();
-}
-
-std::vector<Vector3ui> DenseGrid::getOccupiedCoords() const
-{
-    return voxelmap::ProbVoxelMap::getOccupiedCoords();
-}
-
-
-float DenseGrid::getVoxelSideLength() const
-{
-    return voxelmap::ProbVoxelMap::getVoxelSideLength();
-}
-
-Vector3ui DenseGrid::getDimensions() const
-{
-    return voxelmap::ProbVoxelMap::getDimensions();
-}
-
-bool DenseGrid::writeToDisk(const std::string& path)
-{
-    return voxelmap::ProbVoxelMap::writeToDisk(path);
-}
-
-bool DenseGrid::readFromDisk(const std::string& path)
-{
-    return voxelmap::ProbVoxelMap::readFromDisk(path);
+void DenseGrid::copyIthOccupied(const DenseGrid* other, unsigned long copy_index) const {
+  voxelmap::ProbVoxelMap::copyIthOccupied(other, copy_index);
 }
